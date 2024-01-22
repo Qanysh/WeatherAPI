06d69d64c5e823246d6d0ad13646cbaaff5b7b33
Qanysh <220667@astanait.edu.kz>
2024-01-22 22:47:18 +0600

    update2

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# WeatherAPI
-WeatherAPI
+# Assignment 2
+
+### Installation
+
+-   Clone the repository `git clone https://github.com/Qanysh/WeatherAPI`
+-   Install dependencies `npm i`
+-   Run the server usign dev script `npm start`
+
+### Used npm dependencies
+
+-   [express](https://www.npmjs.com/package/express) - easy and fast js framework
+-   [nodemon](https://www.npmjs.com/package/nodemon) - automatically restarts the node application when file changes (developers dependency)
+
+### Used APIs and Libraries
+
+-   [Google Fonts CSS](https://fonts.google.com/) - fonts
+-   [Bootstrap 5 CSS](https://getbootstrap.com/) - css framework
+-   [Sweet Alerts 2](https://sweetalert2.github.io/) - new alerts
+-   
+-   [Google Maps JS API](https://maps.googleapis.com/) - maps service
+-   [Google Geocoder](https://developers.google.com/maps/documentation/geocoding/overview) - city name by coordinates
+-   [Geoapify Geocoding API](https://www.geoapify.com/) - coordinates by city name
+-   [Open Weather Map API](https://openweathermap.org/) - weather data api
+<<<<<<< HEAD
+-   [Google Material Icons API](https://fonts.google.com/icons) - icons
+=======
+-   [Google Material Icons API](https://fonts.google.com/icons) - icons
+>>>>>>> dc9025c9e3f9bab14ffb96e583d3d61c29622de8
--- server.js
+++ server.js
@@ -1,15 +1,117 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.get("/api/coordinates", async (req, res) => {
+    try {
+        const response = await fetch(
+            `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
+                req.query.search
+            )}&apiKey=39bf8df9317f48bc822123c7b2419c96`
+        );
+
+        const result = await response.json();
+
+        if (result.features && result.features.length > 0) {
+            res.json({ lat: result.features[0].properties.lat, lon: result.features[0].properties.lon });
+        } else {
+            res.status(404);
+        }
+    } catch (error) {
+        console.error("error", error);
+        res.status(500).send("error");
+    }
+<<<<<<< HEAD
+=======
+});
+
+app.get("/api/weather", async (req, res) => {
+    const lat = parseFloat(req.query.lat);
+    const long = parseFloat(req.query.long);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=75ae233af45a22eadf235214a7ab9d18`;
+
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+        res.status(response.status).send("error");
+        return;
+    }
+
+    const data = await response.json();
+    const kelvin = 273.15;
+
+    res.json({
+        temperature: Math.floor(data.main.temp - kelvin),
+        feels_like: Math.floor(data.main.feels_like - kelvin),
+        humidity: data.main.humidity,
+        temp_min: Math.floor(data.main.temp_min - kelvin),
+        temp_max: Math.floor(data.main.temp_max - kelvin),
+        pressure: data.main.pressure,
+        visibility: data.visibility,
+        wind_speed: data.wind.speed,
+        wind_deg: data.wind.deg,
+        clouds: data.clouds.all,
+        sunrise: new Date(data.sys.sunrise * 1000).toLocaleTimeString(),
+        sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString(),
+        name: data.weather[0].main,
+    });
+});
+
+app.get("/map", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "map.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+>>>>>>> dc9025c9e3f9bab14ffb96e583d3d61c29622de8
+});
+
+app.get("/api/weather", async (req, res) => {
+    const lat = parseFloat(req.query.lat);
+    const long = parseFloat(req.query.long);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=75ae233af45a22eadf235214a7ab9d18`;
+
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+        res.status(response.status).send("error");
+        return;
+    }
+
+    const data = await response.json();
+    const kelvin = 273.15;
+
+    res.json({
+        temperature: Math.floor(data.main.temp - kelvin),
+        feels_like: Math.floor(data.main.feels_like - kelvin),
+        humidity: data.main.humidity,
+        temp_min: Math.floor(data.main.temp_min - kelvin),
+        temp_max: Math.floor(data.main.temp_max - kelvin),
+        pressure: data.main.pressure,
+        visibility: data.visibility,
+        wind_speed: data.wind.speed,
+        wind_deg: data.wind.deg,
+        clouds: data.clouds.all,
+        sunrise: new Date(data.sys.sunrise * 1000).toLocaleTimeString(),
+        sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString(),
+        name: data.weather[0].main,
+    });
+});
+
+app.get("/map", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "map.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
